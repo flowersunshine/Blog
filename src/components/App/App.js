8c651ad361b756d089ded33405e072800af9123d
王肖添 <wangxiaotian@doumi.com>
2018-08-25 17:31:04 +0800

    inti

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
-import logo from '../../logo.svg';
-import './App.css';
-import { Layout } from 'antd';
+import React, { Component } from 'react'
+import logo from '../../logo.svg'
+import './App.css'
+import { Layout } from 'antd'
 import  SiderRecommend from '../sider/Sider'
-import { PostList } from '../PostList/PostList';
-import { getPostList } from '../../axios/axios';
+import { PostList } from '../PostList/PostList'
+import { getPostList } from '../../axios/axios'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { Author } from '../author/author';
 
 const { Header, Footer, Sider, Content } = Layout;
 
@@ -27,20 +29,25 @@ class App extends Component {
   
   render() {
     return (
-      <Layout>
-        <Header>
-          <img src={logo} alt="logo" className="logo"></img>
-          <div className="nav-bar">
-            <a>文章列表</a>
-            <a>作者简介</a>
-          </div>
-        </Header>
+      <Router>
         <Layout>
-          {/* <Content><PostList></PostList></Content> */}
-          <Sider><SiderRecommend></SiderRecommend></Sider>
+          <Header>
+            <img src={logo} alt="logo" className="logo"></img>
+            <div className="nav-bar">
+              <Link to="/">文章列表</Link>
+              <Link to="/author">作者简介</Link>
+            </div>
+          </Header>
+          <Layout>
+            <Content>
+                <Route path="/1" component={PostList}></Route>
+                <Route path="/author" component={Author}></Route>
+            </Content>
+            <Sider><SiderRecommend></SiderRecommend></Sider>
+          </Layout>
+          <Footer><p className="footer">本Blog的最终解释权由flowersunshine所有，如有问题可以邮件<a href="mailto:wang_xiao_tian@126.com">wang_xiao_tian@126.com</a></p></Footer>
         </Layout>
-        <Footer>本Blog的最终解释权由flowersunshine所有，如有问题可以邮件<a href="mailto:wang_xiao_tian@126.com">wang_xiao_tian@126.com</a></Footer>
-      </Layout>
+      </Router>
     )
   }
 }
--- src/components/author/author.js
+++ src/components/author/author.js
@@ -3,7 +3,11 @@ import React from 'react'
 export function Author(){
     return(
         <div>
-            
+            <img src="" className="avator"></img>
+            <div className="person-desc">
+                <header className="person-name">王肖添</header>
+                <p className="person-info">老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了老牛逼了</p>
+            </div>
         </div>
     )
 }
