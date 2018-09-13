2deff62c6e5f1e790984edda92fd512c4e0e9a5b
王肖添 <wangxiaotian@doumi.com>
2018-09-13 16:11:50 +0800

    reducer简单的开发完成

--- src/reducers/addBriefReducer.js
+++ src/reducers/addBriefReducer.js
@@ -1,15 +1,26 @@
-import { } from '../constant/actionTypeConstant'
+import { addCommentFooter, addRead, addLike } from '../constant/actionTypeConstant'
 
 function addBriefReducer(state = {}, action){
+    const temp = state[action.articleId];
     switch (action.type){
         case addCommentFooter : {
-            const temp = state[action.articleId];
             return {...state, ...{[action.articleId]:
             {read: temp.read, like: temp.like, comment: temp.comment + 1}}};
         }
-        case add
+        case addRead : {
+            return {...state, ...{[action.articleId]:
+                {read: temp.read + 1, like: temp.like, comment: temp.comment}}};
+        }
+        case addLike : {
+            return {...state, ...{[action.articleId]:
+                {read: temp.read, like: temp.like + 1, comment: temp.comment}}}; 
+        }
         default : {
             return state;
         }
     }
+}
+
+export default {
+    addBriefReducer
 }
